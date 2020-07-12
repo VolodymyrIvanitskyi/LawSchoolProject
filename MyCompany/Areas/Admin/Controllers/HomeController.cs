7bae4345d18786bf5071ee5fa44d9513cb4ca014
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-07-12 17:10:51 +0300

    добавлено html для Intex.cshtml для панелі адміністратора

--- MyCompany/Areas/Admin/Controllers/HomeController.cs
+++ MyCompany/Areas/Admin/Controllers/HomeController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
 
 namespace MyCompany.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
         {
-            return View();
+            this.dataManager = dataManager;
+        }
+            public IActionResult Index()
+        {
+            return View(dataManager.ServiceItems.GetServiceItems());
         }
     }
 }
