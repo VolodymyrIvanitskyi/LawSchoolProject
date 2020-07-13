5e2debe7d4ff349e7ec4aa11a4dc283102648dd5
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-07-13 10:23:33 +0300

    CKeditor відобрадається в панелі адміністратора

---
+++ MyCompany/Areas/Admin/Controllers/ServiceItemsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace MyCompany.Areas.Admin.Controllers
+{
+    public class ServiceItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
