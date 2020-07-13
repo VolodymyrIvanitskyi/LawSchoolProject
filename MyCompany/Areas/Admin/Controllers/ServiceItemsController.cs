3f993074e1acb67cec6c84ae79a442d24d1ff1c7
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-07-13 15:34:05 +0300

    В панелі адміністратора добавлена можливість редагувати "Головну сторінку" та "Наші послуги"(Наші курси)

--- MyCompany/Areas/Admin/Controllers/ServiceItemsController.cs
+++ MyCompany/Areas/Admin/Controllers/ServiceItemsController.cs
@@ -4,14 +4,54 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using MyCompany.Domain.Entities;
+using MyCompany.Domain;
+using MyCompany.Service;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace MyCompany.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ServiceItemsController : Controller
     {
-        public IActionResult Index()
+        private readonly DataManager dataManager;
+        private readonly IWebHostEnvironment hostingEnvironment;
+
+        public ServiceItemsController(DataManager dataManager, IWebHostEnvironment hostingEnvironment)
         {
-            return View();
+            this.dataManager = dataManager;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Edit(Guid id)
+        {
+            var entity = id == default ? new ServiceItem() : dataManager.ServiceItems.GetServiceItemById(id);
+            return View(entity);    
+        }
+        [HttpPost]
+        public IActionResult Edit(ServiceItem model, IFormFile titleImageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if(titleImageFile != null)
+                {
+                    model.TitleImagePath = titleImageFile.FileName;
+                    using (var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath,"images/",titleImageFile.FileName), FileMode.Create))
+                    {
+                        titleImageFile.CopyTo(stream);
+                    }
+                }
+                dataManager.ServiceItems.SaveServiceItem(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete (Guid id)
+        {
+            dataManager.ServiceItems.DeleteServiceItem(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
         }
     }
 }
